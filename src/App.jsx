76a6dd8c4088c25eeae9b78c9519cf78d024d499
兰海 <lan.hai@z2a.com.cn>
2020-06-19 15:40:56 +0800

    配置sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Button } from 'antd-mobile';
 
+import './scss/App.scss';
+
 export default class App extends Component {
   constructor() {
     super();
     this.state = {};
   }
   render() {
-    return <Button type="primary">primary</Button>;
+    return (
+      <div className="box">
+        <Button type="primary">primary</Button>
+        <p>配置scss</p>
+      </div>
+    );
   }
 }
